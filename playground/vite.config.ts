64017b4b8cf078f17ea005125b820d87d0d3f1f4
wobsoriano <sorianorobertc@gmail.com>
2023-08-11 12:01:27 -0700

    update dev vite config

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,6 +2,14 @@ import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [basicSsl(), solid()],
+export default defineConfig((config) => {
+  if (config.command === 'serve') {
+    return {
+      plugins: [basicSsl(), solid()],
+    }
+  }
+
+  return {
+    plugins: [solid()],
+  }
 })
