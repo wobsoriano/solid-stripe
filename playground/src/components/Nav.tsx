b2e1bf0806752b6d7755e8d0451298ae19ca3067
wobsoriano <sorianorobertc@gmail.com>
2023-08-12 10:45:13 -0700

    add github link

--- playground/src/components/Nav.tsx
+++ playground/src/components/Nav.tsx
@@ -1,32 +1,31 @@
-import { JSXElement } from "solid-js";
-import { A } from "solid-start";
+import type { JSXElement } from 'solid-js'
+import { A } from 'solid-start'
 
-export default function Nav({ children }: { children: JSXElement }) {
+export default function Nav(props: { children: JSXElement }) {
   return (
     <div class="drawer lg:drawer-open">
-      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col">
         <div class="w-full navbar bg-base-300">
           <div class="flex-none lg:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
             </label>
-          </div> 
+          </div>
           <div class="flex-1 px-2 mx-2">Solid Stripe</div>
           <div class="flex-none hidden lg:block">
             <ul class="menu menu-horizontal">
-              <li><a>Navbar Item 1</a></li>
-              <li><a>Navbar Item 2</a></li>
+              <li><a href="https://github.com/wobsoriano/solid-stripe" target="_BLANK">GitHub</a></li>
             </ul>
           </div>
         </div>
         <div class="p-4">
-          {children}
+          {props.children}
         </div>
-      </div> 
+      </div>
       <div class="drawer-side">
-        <label for="my-drawer-3" class="drawer-overlay"></label> 
+        <label for="my-drawer-3" class="drawer-overlay" />
         <ul class="menu p-4 w-80 h-full bg-base-200">
           <li><A href="/">Home</A></li>
           <li><A href="/credit-card">Credit cards</A></li>
