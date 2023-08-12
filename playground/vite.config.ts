cc5fac6cc70ee8bffa0a295a7585d3fea702174b
wobsoriano <sorianorobertc@gmail.com>
2023-08-12 11:43:12 -0700

    ci: use vercel adapter

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,15 +1,16 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import vercel from 'solid-start-vercel'
 
 export default defineConfig((config) => {
   if (config.command === 'serve') {
     return {
-      plugins: [basicSsl(), solid()],
+      plugins: [basicSsl(), solid({ adapter: vercel() })],
     }
   }
 
   return {
-    plugins: [solid()],
+    plugins: [solid({ adapter: vercel() })],
   }
 })
