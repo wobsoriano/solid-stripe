708a33dd2487774a5ef85121852d041d2a8dac1c
wobsoriano <sorianorobertc@gmail.com>
2023-09-04 23:20:31 -0700

    docs: add solid integration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import solid from '@astrojs/solid-js';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		solid(),
 		starlight({
 			title: 'My Docs',
 			social: {
